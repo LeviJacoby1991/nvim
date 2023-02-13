a21fd46f165d5a06026e61b6978809a991762e14
Levi <levi.jacoby@fathom5.co>
2023-02-13 15:30:54 -0600

    Fixed toggle term keymaps

--- lua/users/levi/toggle_term.lua
+++ lua/users/levi/toggle_term.lua
@@ -1,7 +1,7 @@
 toggle_term = require("toggleterm")
 
 function _G.set_terminal_keymaps()
-  local opts = {buffer = nr}
+  local opts = {buffer = 0}
   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', 'jj', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
@@ -10,4 +10,5 @@ function _G.set_terminal_keymaps()
   vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
 end
 
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 toggle_term.setup{}
