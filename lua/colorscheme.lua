29fe974fa9c5e81b7dc06296a20188b71358709f
Levi <levi.jacoby1991@gmail.com>
2024-02-12 22:09:47 -0600

    Moved plugin manager to lazy, the times they are a changing. Note remove packer stuff on your work laptop

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,10 +1,9 @@
-local status_ok, gruvbox = pcall(require, "gruvbox")
+local status_ok, gruvbox = pcall(require, 'gruvbox')
 
 if not status_ok then
   return
 end
 
--- fdasdf
 gruvbox.setup({
   italic = {
     strings = false,
