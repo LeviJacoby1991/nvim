ba8e27d9a3cd33b1b1a3987bac4a4ef293ca6438
Levi <levi.jacoby1991@gmail.com>
2024-02-12 22:37:13 -0600

    my obsession over removing "" continues

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,7 +1,7 @@
 local function map(mode, lhs, rhs, opts)
 	local options = { noremap = true }
 	if opts then
-		options = vim.tbl_extend("force", options, opts)
+		options = vim.tbl_extend('force', options, opts)
 	end
 	vim.keymap.set(mode, lhs, rhs, options)
 end
