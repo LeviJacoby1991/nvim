5873a80f87d70e6282ca26ed85438fa84838b0f2
Levi <levi.jacoby1991@gmail.com>
2025-08-16 14:26:58 -0500

    switched form neodev to lazydev for nvim lsp source

--- lua/neodev.lua
+++ lua/neodev.lua
@@ -1,18 +1,13 @@
-local status_ok, neodev = pcall(require, 'neodev')
-
-if not status_ok then
-  return
-end
-
-neodev.setup({
-    library = {
+return {
+  'folke/neodev.nvim',
+  library = {
     enabled = true,
     runtime = true,
     types = true,
     plugins = true,
   },
   setup_jsonls = true,
-  override = function(root_dir, options) end,
+  override = function (root_dir, options) end,
   lspconfig = true,
   pathStrict = true,
-})
+}
